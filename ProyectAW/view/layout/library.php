c5c41be17b78b81421d9b357dd92e754b36b3963
Enrique Roman <aeromandiaz@gmail.com>
2018-05-06 23:07:43 +0200

    ahora si

--- ProyectAW/view/layout/library.php
+++ ProyectAW/view/layout/library.php
@@ -5,6 +5,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="Madeliciosa Web">
 <meta name="author" content="Carlos Olano">
-<link rel="stylesheet" href="css\index_view.css"  type="text/css" media="all" />
 </head>
 <body>
